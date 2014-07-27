820f3541f1d84f3e5166f0964615824277a6c62b
Cyrus Eyster <cyruse@alum.mit.edu>
2014-07-27 12:48:41 -0400

    No longer print when using the cache. Changed from double hash to single hash in comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,13 @@
-## These functions allow the user to cache the result
-## of a matrix inverse operation, so that that result
-## can be looked up in the cache (which is fast), rather than
-## recalculated (which is slow).
-##
-## This code is based on the mean-caching code which was
-## provided with this assignment.
+# These functions allow the user to cache the result
+# of a matrix inverse operation, so that that result
+# can be looked up in the cache (which is fast), rather than
+# recalculated (which is slow).
+#
+# This code is based on the mean-caching code which was
+# provided with this assignment.
 
-## This function creates an object which can cache the
-## inverse of a matrix.
+# This function creates an object which can cache the
+# inverse of a matrix.
 makeCacheMatrix <- function(x = matrix()) {
     inverse <- NULL
     set <- function(y) {
@@ -22,18 +22,19 @@ makeCacheMatrix <- function(x = matrix()) {
          getinv = getinv)
 }
 
-## This function will return the inverse of a matrix
-## that has been set up to have a cacheable inverse
-## using the previous function.
-##
-## If that inverse has already been calculated, then
-## the cached inverse value will be returned. Otherwise,
-## the value will be calculated and cached.
+# This function will return the inverse of a matrix
+# that has been set up to have a cacheable inverse
+# using the previous function.
+#
+# If that inverse has already been calculated, then
+# the cached inverse value will be returned. Otherwise,
+# the value will be calculated and cached.
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
+    # Return a matrix that is the inverse of 'x'
     cached_inverse <- x$getinv()
     if(!is.null(cached_inverse)) {
-        message("Getting cached data")
+        # This is nice for debugging, but not necessary otherwise:
+        #message("Getting cached data")
         return(cached_inverse)
     }
     matrix <- x$get()
